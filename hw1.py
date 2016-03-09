05f167e5ed2466328bc68e7dfe5a4c490fec11de
bigcanbigbig <jupiter2432321@gmail.com>
2016-03-09 21:48:19 +0800

    Added files via upload

--- hw1.py
+++ hw1.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+num=raw_input('你要領多少錢呢:')
+want=int(num)
+money=5000
 
-print "資管107級\n"
-print "A1033337\n"
-print "余冠柔 Zona Yu\n"
+f=open("ATM.txt","a")
+
+if money>want:
+    print "你的存款還剩%d元" %(money-want)
+    f.write("你的存款還剩%d元\n"%(money-want))
+elif want==money:
+    print "你沒錢了"
+    f.write("你沒錢了\n")
+else:
+    print "錢不夠啦笨蛋"
+    f.write("錢不夠啦笨蛋\n")
+
+f.close
